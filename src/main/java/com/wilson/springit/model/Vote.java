e820f9aee5b6725c242defceaa4bdceaef977314
wuchau <wuchau@connect.ust.hk>
2021-01-20 14:07:54 +0800

    created entities and refactored using project lombok

---
+++ src/main/java/com/wilson/springit/model/Vote.java
@@ -0,0 +1,26 @@
+package com.wilson.springit.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Vote {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int vote;
+
+
+    public Vote(){}
+
+
+
+}
